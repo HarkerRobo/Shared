156f033fee77b8debd422809f6fc19e1a372ab3c
Joel Manning <stopwatchj@gmail.com>
2017-01-25 15:44:54 -0800

    Initial Commit

---
+++ Respondable.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team1072.shared;
+
+/**
+ * @author joelmanning
+ *
+ */
+public interface Respondable {
+	public void send(Object... objs);
+	public String getIP();
+}
