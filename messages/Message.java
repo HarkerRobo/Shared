c1ebb5bd0b341578d9733ecc00508721f6b92877
Joel Manning <stopwatchj@gmail.com>
2017-01-27 11:23:22 -0800

    Generated ID

--- messages/Message.java
+++ messages/Message.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team1072.shared.messages;
 import java.io.Serializable;
 public abstract class Message implements Serializable {
-	public static final long serialVersionUID = 0L;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -444995760188308861L;
+	
 }
